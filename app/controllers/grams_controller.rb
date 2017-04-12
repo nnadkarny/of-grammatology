0eac8dd6fde2ad17aca13724d5c989f2a86e605f
“Nikhil <nnadkarny@gmail.com>
2017-04-12 18:38:55 +0000

    5.4.13 (placeholder)

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -24,6 +24,13 @@ class GramsController < ApplicationController
     end
   end
 
+  def edit
+    @gram = Gram.find_by_id(params[:id])
+    if @gram.blank?
+      render plain: 'Not Found :(', status: :not_found
+    end
+  end
+
   private
 
   def gram_params
