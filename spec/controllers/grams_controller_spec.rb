4e38fbce7bd45d857c51d995a303df93e14d43fa
“Nikhil <nnadkarny@gmail.com>
2017-04-12 18:26:57 +0000

    5.4.13 (placeholder)

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -4,11 +4,14 @@ RSpec.describe GramsController, type: :controller do
 
   describe "grams#edit action" do
     it "should successfully show the edit form if the gram is found" do
-
+      gram = FactoryGirl.create(:gram)
+      get :edit, params: { id: gram.id }
+      expect(response).to have_http_status(:success)
     end
 
     it "should return a 404 error message if the gram is not found" do
-
+      get :edit, params: { id: 'MOSCATO' }
+      expect(response).to have_http_status(:not_found)
     end
   end
 
