f43567961a20591f73d5fdaa5bba0aefff5014af
“Nikhil <nnadkarny@gmail.com>
2017-04-12 20:04:14 +0000

    5.4.13 Edit and Update gram

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -6,9 +6,7 @@ class GramsController < ApplicationController
 
   def show
     @gram = Gram.find_by_id(params[:id])
-    if @gram.blank?
-      render plain: 'Not Found :(', status: :not_found
-    end
+    return render_not_found if @gram.blank?
   end
 
   def new
@@ -17,6 +15,7 @@ class GramsController < ApplicationController
 
   def create
     @gram = current_user.grams.create(gram_params)
+
     if @gram.valid?
       redirect_to root_path
     else
@@ -26,8 +25,19 @@ class GramsController < ApplicationController
 
   def edit
     @gram = Gram.find_by_id(params[:id])
-    if @gram.blank?
-      render plain: 'Not Found :(', status: :not_found
+    return render_not_found if @gram.blank?
+  end
+
+  def update
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+
+    @gram.update_attributes(gram_params)
+
+    if @gram.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
@@ -35,5 +45,9 @@ class GramsController < ApplicationController
 
   def gram_params
     params.require(:gram).permit(:message)
+  end
+
+  def render_not_found
+    render plain: 'Not Found :(', status: :not_found
   end
 end
