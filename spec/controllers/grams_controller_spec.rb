97bb62976f003d669bff48e519cff53aa0ab03df
“Nikhil <nnadkarny@gmail.com>
2017-04-12 20:35:44 +0000

    5.4.14 Destroying grams

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -2,11 +2,28 @@ require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
 
+  describe "grams#destroy action" do
+    it "should allow a user to destroy grams" do
+      gram = FactoryGirl.create(:gram)
+      delete :destroy, params: { id: gram.id }
+      expect(response).to redirect_to root_path
+
+      gram = Gram.find_by_id(gram.id)
+      expect(gram).to eq nil
+    end
+
+    it "should return a 404 message if we cannot find a gram with the id that is specified" do
+      delete :destroy, params: { id: 'SHEMP' }
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
   describe "grams#update action" do
     it "should allow users to successfully update grams" do
       gram = FactoryGirl.create(:gram, message: "Initial Value")
       patch :update, params: { id: gram.id, gram: { message: 'Changed' } }
       expect(response).to redirect_to root_path
+      
       gram.reload
       expect(gram.message).to eq "Changed"
     end
@@ -20,6 +37,7 @@ RSpec.describe GramsController, type: :controller do
       gram = FactoryGirl.create(:gram, message: "Initial Value")
       patch :update, params: { id: gram.id, gram: { message: '' } }
       expect(response).to have_http_status(:unprocessable_entity)
+      
       gram.reload
       expect(gram.message).to eq "Initial Value"
     end
