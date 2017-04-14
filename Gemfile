2123f413bbadc372c0ba5db73ec3bd43b8f526c2
“Nikhil <nnadkarny@gmail.com>
2017-04-14 20:23:48 +0000

    5.5.16/17 CarrierWave/Validating Image

--- Gemfile
+++ Gemfile
@@ -62,3 +62,9 @@ gem 'bootstrap', '~> 4.0.0.alpha6'
 gem 'simple_form'
 
 gem 'devise'
+
+gem 'carrierwave', '~> 1.0'
+
+gem "figaro"
+
+gem "fog"
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -57,4 +57,6 @@ RSpec.configure do |config|
 
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
+
+  include ActionDispatch::TestProcess
 end
